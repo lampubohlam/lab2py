a47428ff78a6955b75c74a18ba3b71be118c261c
lampubohlam <116137169+lampubohlam@users.noreply.github.com>
2022-10-26 22:27:46 +0700

    Add files via upload

---
+++ latihan 2.py
@@ -0,0 +1,14 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello")
+Hello
+>>> print("saya sedang belajar python")
+saya sedang belajar python
+>>> a=8
+>>> b=6
+>>> print("variabel a=",a)
+variabel a= 8
+>>> print("variabel b=",b)
+variabel b= 6
+>>> print("hasil penjumlahan a+b=",a+b)
+hasil penjumlahan a+b= 14
